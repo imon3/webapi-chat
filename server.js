40582e23568d17c7e8d048410f8e774111eb32e4
Imon Ovbude <imonovbude@gmail.com>
2019-02-19 11:53:34 -0800

    finished practice

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const hubsRouter = require('./hubs/hubs-router')
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/hubs', hubsRouter)
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h2>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+
+
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
+
+server.listen(4000, () => {
+  console.log('\n*** Server Running on http://localhost:5000 ***\n');
+});
+
+module.exports = server
